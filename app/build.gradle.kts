61936e70d3d5a2f1395d142483ae03af67bcd6b6
alejandrols22 <alejandrolopsep@gmail.com>
2023-12-30 12:13:55 +0100

    Desarrollo de barra principal de busqueda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation ("androidx.appcompat:appcompat:1.3.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation ("com.google.android.material:material:1.4.0")
     implementation ("com.google.android.material:material:<última-versión>")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
